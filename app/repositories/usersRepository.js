a8c79e97c7e7eeb64c8c10fecea5be34190ca743
Alexandra Pop <alexandrapop@WS-CJ-U2-2570.local>
2017-12-21 17:54:31 +0200

    Fixed issue with mongo errors for async/await.

--- app/repositories/usersRepository.js
+++ app/repositories/usersRepository.js
@@ -2,50 +2,26 @@ const mongoose = require( "mongoose" );
 
 const User = mongoose.model( "User" );
 
-const findUser = async ( id ) => {
-    try {
-        const queryResult = await User.findOne( { id } );
-        return queryResult;
-    } catch ( err ) {
-        return err;
-    }
-};
+const findUser = async ( id ) => User.findOne( { id } );
 
 const saveUser = async ( data ) => {
-    try {
-        const user = new User( data );
-        user.setPass( data.password );
-        const queryResult = await user.save( );
-
-        return queryResult;
-    } catch ( err ) {
-        return err;
-    }
+    const user = new User( data );
+    user.setPass( data.password );
+    return user.save( );
 };
 
 const editUser = async ( userObject, newData ) => {
     const { name, sex, age } = newData;
     const user = userObject;
-    try {
-        user.name = name;
-        user.sex = sex;
-        user.age = age;
 
-        const queryResult = await user.save( );
-        return queryResult;
-    } catch ( err ) {
-        return err;
-    }
+    user.name = name;
+    user.sex = sex;
+    user.age = age;
+
+    return user.save( );
 };
 
-const deleteUser = async ( user ) => {
-    try {
-        const queryResult = await user.remove();
-        return queryResult;
-    } catch ( err ) {
-        return err;
-    }
-};
+const deleteUser = async ( user ) => user.remove();
 
 module.exports = {
     findUser,

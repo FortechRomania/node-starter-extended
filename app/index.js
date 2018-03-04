2ff013af515cca37be0323831f92a58783e24f1a
Cristian Leu <cristianleu@188-24-4-237.rdsnet.ro>
2018-03-04 19:03:55 +0200

    initial GraphQl functions

--- app/index.js
+++ app/index.js
@@ -23,6 +23,16 @@ app.use( bodyParser.json( ) );
 app.use( customResponses );
 app.use( helmet() );
 
+app.use( "/graphql", ( req, res, next ) => {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With" );
+    if ( req.method === "OPTIONS" ) {
+        res.sendStatus( 200 );
+    } else {
+        next();
+    }
+} );
+
 app.use( "/graphql", expressGraphQL( {
     schema: testSchema,
     graphiql: true,

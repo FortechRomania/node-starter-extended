a1fa52913fbb2825f1447e1bcff6fdea405210e1
Birtalan Robert <robert.birtalan@fortech.ro>
2018-09-05 17:17:20 +0300

    unique user added to User schema, and X-Access-Token added to accepted headers list

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ require( "./models/user" );
 
 app.use( ( req, res, next ) => {
     res.header( "Access-Control-Allow-Origin", "*" );
-    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token" );
     next();
 } );
 
